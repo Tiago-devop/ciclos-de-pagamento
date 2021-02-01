d88f60cf0813b142aea9dcb1f80b3d3eaf6e9396
Tiago-devop <tiagodev@protonmail.com>
2021-02-01 14:57:51 -0300

    footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default (props) => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2021
+      <a
+        href="https://www.linkedin.com/in/tiago-ramos-7780831a3/"
+        target="_blank"
+      >
+       {" "} Tiago developer
+      </a>
+      .
+    </strong>
+  </footer>
+)
