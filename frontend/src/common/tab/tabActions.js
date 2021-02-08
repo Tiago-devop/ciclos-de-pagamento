10644f508f670d87c2937e77659dc41f35d68028
Tiago-devop <tiagodev@protonmail.com>
2021-02-08 16:17:05 -0300

    bug fixed on showTabs

--- frontend/src/common/tab/tabActions.js
+++ frontend/src/common/tab/tabActions.js
@@ -7,7 +7,7 @@ export function selectTab(tabId) {
 
 export function showTabs(...tabIds) {
   const tabsToShow = {};
-  tabIds.forEach((e) => (tabIds[e] = true));
+  tabIds.forEach((e) => (tabsToShow[e] = true));
   return {
     type: "TAB_SHOWED",
     payload: tabsToShow,
